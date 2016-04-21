f141868aca0e74129b9f496346d832d336997d61
Scott Diallo <diallot@me.com>
2016-04-21 08:57:36 -0700

    api setup

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$('document').ready(function(){
+    function getCalendar(userInput){
+        var myCalendar = {
+            url :'https://calendar.google.com/calendar/render?tab=mc#main_7%7Cmonth-3+23644+23682+23649',
+            dataType: 'json',
+            data:{
+                key:'1041767674591-4lbrsrsii54h0lgak6a28lae4eo4e24e.apps.googleusercontent.com'
+            }
+        };
+    }
+});
